7b592453dcdf732f5449e7e35df73dbc18dbcdb1
EmberFox <161511358+EmberFox1221@users.noreply.github.com>
2025-12-10 17:24:40 -0600

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -155,7 +155,7 @@ ALLOW_MISSING_DEPENDENCIES := true
 USE_RECOVERY_INSTALLER := true
 RECOVERY_INSTALLER_PATH := bootable/recovery/installer
 TW_EXCLUDE_TWRPAPP := true
-TW_INCLUDE_REPACKTOOLS := false
+TW_INCLUDE_REPACKTOOLS := true
 TW_HAS_EDL_MODE := true
 
 # Workaround for error copying vendor files to recovery ramdisk
