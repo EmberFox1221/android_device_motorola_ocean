b7ae9b4d3acb4f348da56e67e0ee26f50fbc4065
EmberFox <161511358+EmberFox1221@users.noreply.github.com>
2025-12-10 14:02:37 -0600

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,14 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-TW_EXCLUDE_BASH := true
-TW_EXCLUDE_NANO := true
-TW_EXCLUDE_HTOP := true
-TW_EXCLUDE_TAR := true
-TW_EXCLUDE_TZDATA := true
-TW_EXCLUDE_PYTHON := true
-TW_EXCLUDE_ZIP := true
-TW_EXCLUDE_EMOJI_FONT := true
+
 
 DEVICE_PATH := device/motorola/ocean
 
@@ -177,3 +170,11 @@ BOARD_RAMDISK_USE_LZMA := true
 TW_EXCLUDE_PYTHON := true
 OF_DISABLE_TREBLE_COMPATIBILITY_CHECK := 1
 
+TW_EXCLUDE_BASH := true
+TW_EXCLUDE_NANO := true
+TW_EXCLUDE_HTOP := true
+TW_EXCLUDE_TAR := true
+TW_EXCLUDE_TZDATA := true
+TW_EXCLUDE_PYTHON := true
+TW_EXCLUDE_ZIP := true
+TW_EXCLUDE_EMOJI_FONT := true
